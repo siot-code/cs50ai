8377b860245f7330d3c85898b88701051b4316b6
othmane.sibaba <othmane.sibaba@bou6-solutions.com>
2025-10-17 12:02:19 +0200

    finalized knight

--- knowledge/knights/src/puzzle.py
+++ knowledge/knights/src/puzzle.py
@@ -12,21 +12,36 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+   Or(AKnave, AKnight),
+   Not(And(AKnave,AKnight)),
+   Implication(AKnight,And(AKnight,AKnave)),
+   Implication(AKnave,Not(And(AKnight,AKnave))),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+   Or(AKnave, AKnight),
+   Or(BKnave, BKnight),
+   Not(And(AKnight,AKnave)),
+   Not(And(BKnight,BKnave)),
+   Implication(AKnight,And(AKnave,BKnave)),
+   Implication(AKnave,Not(And(AKnave,BKnave))),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+   Or(AKnave, AKnight),
+   Or(BKnave, BKnight),
+   Not(And(AKnight,AKnave)),
+   Not(And(BKnight,BKnave)),
+   Implication(AKnight,Or(And(AKnight,BKnight),And(AKnave,BKnave))),
+   Implication(AKnave,Not(Or(And(AKnight,BKnight),And(AKnave,BKnave)))),
+   Implication(BKnight,Or(And(AKnight,BKnave),And(AKnave,BKnight))),
+   Implication(BKnave,Not(Or(And(AKnight,BKnave),And(AKnave,BKnight))))
 )
 
 # Puzzle 3
@@ -35,7 +50,20 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+     Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+
+    # If B is a knight, then C is a knave and A is not a knight
+    Implication(BKnight, And(CKnave, BKnave)),
+    Implication(BKnave, Not(And(CKnave, BKnave))),
+
+    # C says: "A is a knight"
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
 )
 
 
