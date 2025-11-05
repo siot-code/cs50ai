2e9f258cae7570d0f17af2ec86382cbb40ea009a
othmane.sibaba <othmane.sibaba@bou6-solutions.com>
2025-11-05 13:54:54 +0100

    final state

--- neural/traffic/traffic.py
+++ neural/traffic/traffic.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 10
+EPOCHS = 20
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
@@ -58,7 +58,22 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images_list = []   
+    labels_list = []
+    for folder_name in os.listdir(data_dir):                 
+        folder_path = os.path.join(data_dir, folder_name)
+        if os.path.isdir(folder_path):              
+            for filename in os.listdir(folder_path):    
+                image_path = os.path.join(folder_path, filename)
+                image = cv2.imread(image_path)
+                if image is None: 
+                    continue
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image_resized = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT)) / 255.0
+                images_list.append(image_resized)
+                labels_list.append(int(folder_name))
+            
+    return (images_list, labels_list)        
 
 
 def get_model():
@@ -67,8 +82,40 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    
 
+    model = tf.keras.models.Sequential([
+        #newer keras version preferes starting with the input shape first
+        tf.keras.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3)), 
+
+        #convolutional layer 32 filters with 3x3 kernel as in lecture example
+        tf.keras.layers.Conv2D( 32, (3,3), activation="relu"),
+
+        #max pooling layer to zip image using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+
+        #convolutional layer 32 filters with 3x3 kernel as in lecture example
+        tf.keras.layers.Conv2D( 64, (3,3), activation="relu"),
+
+        #max pooling layer to zip image using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+
+        #flatten to 1 dimension vector
+        tf.keras.layers.Flatten(),
+
+        #hidden layer with dropout to avoit relayence on specific nodes
+        tf.keras.layers.Dense(256, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        #as many outpout nodes as there are categories
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+
+
+    ])
+
+    model.compile(optimizer="adam" , loss="categorical_crossentropy", metrics=["accuracy"])
+
+    return model
 
 if __name__ == "__main__":
     main()
